fe920d417c817ed362cd663b8737aafef2c68f29
bini59 <myuni0234@gmail.com>
2023-08-20 12:12:43 +0900

    chore : package.json에 zustand 추가, 상태관리 주석 추가

--- src/store/useStickerStore.js
+++ src/store/useStickerStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export const useStickerStore = create((set) => ({
     id: 1,
     stickers: [],
+
+    // 스티커를 추가하는 함수, id를 통해 스티커를 구분한다.
     addSticker: () => {
         set((state) => ({
             stickers: [
@@ -17,11 +19,17 @@ export const useStickerStore = create((set) => ({
             id: state.id + 1,
         }));
     },
+
+    // 스티커를 제거하는 함수, id를 통해 스티커를 구분한다.
     removeSticker: (id) => {
         set((state) => ({
             stickers: state.stickers.filter((sticker) => sticker.id !== id),
         }));
     },
+
+    // 스티커를 클릭할 때 실행되는 함수.
+    // 스티커의 zIndex를 변경하고, state를 true로 변경한다.
+    // state가 true인 스티커는 크기조절, 이동, 회전, 삭제 등의 기능을 사용할 수 있다.
     onclick: (id) => {
         set((state) => ({
             stickers: state.stickers.map((sticker) => {
