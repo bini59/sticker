9ed78b9b521492a17f74e71c60c5646a05f8df28
bini59 <myuni0234@gmail.com>
2023-08-20 17:35:10 +0900

    feat : onDrag 수정
    - 이미지가 해당 공간 내에 상주하도록 변경

--- src/Components/Previewer.jsx
+++ src/Components/Previewer.jsx
@@ -1,17 +1,28 @@
 import { Sticker } from "./Sticker"
 import { useStickerStore } from "../store/useStickerStore"
+import { useRef } from "react";
 
 export const Previewer = () => {
     const { stickers, onclick, removeSticker } = useStickerStore();
 
+    const containerRef = useRef(null);
+    const containerBounds = {
+        left: 0,
+        top: 0,
+        right: containerRef.current?.clientWidth,
+        bottom: containerRef.current?.clientHeight
+    }
+    
+
     return (
-        <section className="sticker-section">
+        <section className="sticker-section" ref={containerRef}>
             {stickers.map((sticker) => (
 				<Sticker
 					key={sticker.id}
 					src={sticker.src}
 					zIndex={sticker.zIndex}
-					state={sticker.state}
+                    state={sticker.state}
+                    bounds={containerBounds}
 					onclick={() => onclick(sticker.id)}
 					removeSticker={() => removeSticker(sticker.id)}
 				/>
--- src/Components/Sticker.jsx
+++ src/Components/Sticker.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import {
     makeMoveable,
@@ -14,16 +14,57 @@ const Moveable = makeMoveable([
     Rotatable
 ]);
 
-export const Sticker = ({state, zIndex, src, onclick, removeSticker}) => {
+export const Sticker = ({state, zIndex, src, bounds, onclick, removeSticker}) => {
     const [helper] = useState(() => {
         return new MoveableHelper();
     });
     const ref = useRef(null);
+
+    const onDrag = (e) => {
+        // check bounds
+        const { left, top, right, bottom } = bounds;
+        const target = e.target;
+        const translate = e.translate;
+        const { width, height } = e.target.getBoundingClientRect();
+
+        const nowLeft = 100 + translate[0] -
+            (width - target.offsetWidth) / 2;
+        const nowTop = 150 + translate[1] -
+            (height - target.offsetHeight) / 2;
+        const nowRight = nowLeft + width;
+        const nowBottom = nowTop + height;
+
+        console.log(nowLeft)
+        console.log(left-100+(width-target.offsetWidth)/2)
+        console.log(right-width)
+
+
+        
+        if(nowLeft < left) {
+            e.translate[0] = left - 100 + (width - target.offsetWidth) / 2;
+        }
+        if(nowTop < top) {
+            e.translate[1] = top-150+(height-target.offsetHeight)/2;
+        }
+        if (nowRight > right) {
+            e.translate[0] =right - target.offsetWidth - 100 - (width - target.offsetWidth)/2;
+        }
+
+        if (nowBottom > bottom) {
+            e.translate[1] = bottom - target.offsetHeight - 150 - (height - target.offsetHeight)/2;
+        }
+
+        helper.onDrag(e);
+
+
+
+    }
+
     return (
-        <div className="container" style={{zIndex: zIndex, position:"relative"}} >
+        <div className="container" style={{ zIndex: zIndex, position: "relative"}} >
             <div className="target" ref={ref} >
-                <button className="remove" onClick={()=>removeSticker()} style={{display : state ? "block" : "none"}}>X</button>
-                <img onClick={(e) => onclick()} src={src} alt="sticker" width="320" height="240" />
+                <button className="remove" onClick={()=>removeSticker()} style={{display : state ? "block" : "none", fontSize: "20px"}}>✕</button>
+                <img onClick={(e) => onclick()} src={src} alt="sticker" />
             </div>
 
             <Moveable
@@ -31,14 +72,15 @@ export const Sticker = ({state, zIndex, src, onclick, removeSticker}) => {
                 draggable={state}
                 scalable={state}
                 rotatable={state}
-                keepRatio={state}
+                keepRatio={false}
                 onDragStart={helper.onDragStart}
-                onDrag={helper.onDrag}
+                onDrag={onDrag}
                 onScaleStart={helper.onScaleStart}
                 onScale={helper.onScale}
                 onRotateStart={helper.onRotateStart}
                 onRotate={helper.onRotate}
                 hideDefaultLines={!state}
+                bounds={bounds}
             />
         </div>
     )
